6879c9e31d8d05bbdf1f1cde9c8a441a3890f0d3
Luiserebii <luis@serebii.io>
2021-05-10 17:57:30 -0400

    Added documentation to our ETH C API

--- include/eth/eth.h
+++ include/eth/eth.h
@@ -7,30 +7,71 @@ extern "C" {
 
 #include <secp256k1.h>
 
+/**
+ * secp256k1 secret key data type.
+ */
 typedef struct {
     unsigned char data[32];
 } eth_secp256k1_seckey;
 
+/**
+ * secp256k1 uncompressed public key data type.
+ */
 typedef struct {
     unsigned char data[65];
 } eth_secp256k1_unc_pubkey;
 
+/**
+ * secp256k1 keccak256 hash data type.
+ */
 typedef struct {
     unsigned char data[32];
 } eth_keccak256_hash;
 
+/**
+ * ETH keccak256 hash of public key data type. 
+ *
+ * As this is a special kind of a keccak256 hash, it is typedef'd to express 
+ * the unique functions this data type has (e.g. getting address)
+ */
 typedef eth_keccak256_hash eth_pubkey_khash;
 
+/**
+ * Generates a random secp256k1 secret key. Each byte is guaranteed to be in
+ * the range of [0, 2^8-1]. Therefore, the value of the secret key is
+ * guaranteed to be in the range of [0, 2^32-1].
+ *
+ * Note that this does not guarantee a *valid* random secp256k1 key.
+ * See eth_secp256k1_generateseckey.
+ */
 unsigned char* eth_secp256k1_randseckey(eth_secp256k1_seckey* sk);
 
+/**
+ * Generates a random valid secp256k1 secret key. The value of the secret key
+ * is guaranteed to be in the valid range, which is [1, order n of G].
+ */
 void eth_secp256k1_generateseckey(eth_secp256k1_seckey* sk, const secp256k1_context* ctx);
 
+/**
+ * Calculates the uncompressed secp256k1 public key from a secret key.
+ * A secp256k1_context pointer is required.
+ */
 void eth_secp256k1_calculatepubkey(eth_secp256k1_unc_pubkey* pk, const secp256k1_context* ctx, const eth_secp256k1_seckey* sk);
 
+/**
+ * Calculates the keccak256 hash from any arbitrary data.
+ */
 void eth_keccak_calculatepkhash(eth_keccak256_hash* kh, const unsigned char* data);
 
+/**
+ * Calculates the keccak256 hash from a public key. 
+ **/
 void eth_pubkey_khash_calculate(eth_pubkey_khash* kh, const eth_secp256k1_unc_pubkey* pk);
 
+/**
+ * Returns a pointer to the 20-byte segment containing a valid Ethereum address
+ * from public key keccak256 hash.
+ */
 const unsigned char* eth_pubkey_khash_getaddress(const eth_pubkey_khash* kh);
 
 #ifdef __cplusplus
--- planning.md
+++ planning.md
@@ -19,41 +19,6 @@
   * Godbolt - tmux of vim and solc asm output
 
 Just do each function for each key found in file.
-```
-int main() {
-    //Define our 32-bit private key
-    eth_secp256k1_seckey prvkey;
-    //Create secp256k1 context obj
-    secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
-
-    //Generate a valid secret key
-    eth_secp256k1_generateseckey(&prvkey, ctx);
-
-    //Calculate the uncompressed public key
-    eth_secp256k1_unc_pubkey pubkey;
-    eth_secp256k1_calculatepubkey(&pubkey, ctx, &prvkey);
-
-    //Generate hash
-    eth_keccak256_hash khash;
-    eth_pubkey_khash_calculate(&khash, &pubkey);
-
-    secp256k1_context_destroy(ctx);
-
-    char prvstr[500];
-    char pubstr[500];
-    char hashstr[500];
-    bytestohex(prvstr, prvkey.data, 32);
-    bytestohex(pubstr, pubkey.data, 65);
-    bytestohex(hashstr, khash.data, 32);
-    cout << "Private Key: " << prvstr << '\n';
-    cout << "Public Key: " << pubstr << '\n';
-    cout << "Keccak256 Hash: " << hashstr << '\n';
-    //Printing the last 20 bytes, which is the ETH address!
-    cout << "Address: 0x" << (hashstr+24) << '\n';
-}
-
-```
-
 
 ## Implementation
 
