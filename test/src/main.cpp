7e2fe16f48c666fb2e32776a18b5c128cf5b9ca4
Luiserebii <luis@serebii.io>
2021-05-14 07:04:55 -0400

    Experimenting with comparing/evaluating byte array through literal; seems to be failing, maybe a bug?

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,11 +1,18 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#include <string.h>
 #include "../../include/eth/vector-rlp-t.h"
 #include "../../include/eth/rlp.h"
 
 TEST_CASE("ETH Private Key Generation", "[meme]") {
     REQUIRE(1 == 1);
 
+}
+void print_uchar_arr(const unsigned char* arr, size_t sz) {
+    for(size_t i = 0; i < sz; ++i) {
+        printf("%x", arr[i]);
+    }
+    putchar('\n');
 }
 
 TEST_CASE("Testing Struct RLP_T", "[rlp_t]") {
@@ -20,9 +27,14 @@ TEST_CASE("Testing Struct RLP_T", "[rlp_t]") {
 
     SECTION("0xA291 (a valid byte array)") {
         minth_rlp_t_init_from_string(&rlp, "0xA291");
+        unsigned char b[2] = {0xA2, 0x91};
 
         REQUIRE(rlp.tag == MINTH_RLP_T_BYTE_ARR);
         REQUIRE(vector_uchar_size(&rlp.value.byte_array) == 2);
+        unsigned char* arr = vector_uchar_begin(&rlp.value.byte_array); 
+        print_uchar_arr(b, 2);
+        print_uchar_arr(arr, 2);
+        REQUIRE(memcmp(arr, b, 2) == 0);
     }
 
     SECTION("[] (empty list)") {
@@ -46,7 +58,26 @@ TEST_CASE("Testing Struct RLP_T", "[rlp_t]") {
         REQUIRE(e->tag == MINTH_RLP_T_LIST);
         REQUIRE(vector_rlp_t_size(&e->value.list) == 0);
     }
-    minth_rlp_t_init_from_string(&rlp, "[0xA393,0x00]");
+
+    SECTION("[0xA393,0x5CEC20DB,0x00]") {
+        minth_rlp_t_init_from_string(&rlp, "[0xA393,0x5CEC20DB,0x00]");
+
+        REQUIRE(rlp.tag == MINTH_RLP_T_LIST);
+        REQUIRE(vector_rlp_t_size(&rlp.value.list) == 3);
+
+        struct minth_rlp_t* e = vector_rlp_t_begin(&rlp.value.list);
+        REQUIRE(e->tag == MINTH_RLP_T_BYTE_ARR);
+        REQUIRE(vector_uchar_size(&e->value.byte_array) == 2);
+
+        ++e;
+        REQUIRE(e->tag == MINTH_RLP_T_BYTE_ARR);
+        REQUIRE(vector_uchar_size(&e->value.byte_array) == 4);
+
+        ++e;
+        REQUIRE(e->tag == MINTH_RLP_T_BYTE_ARR);
+        REQUIRE(vector_uchar_size(&e->value.byte_array) == 1);
+
+    }
     minth_rlp_t_init_from_string(&rlp, "[0xA393,[],[]]");
     minth_rlp_t_init_from_string(&rlp, "[0xA393,0x843CAB[],[[]]],0xFF]");
 
