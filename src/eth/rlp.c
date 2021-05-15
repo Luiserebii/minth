66660dbeca0fb2d1e3c15f7a2a6b1581c385142e
Luiserebii <luis@serebii.io>
2021-05-15 00:34:23 -0400

    All test cases passing! Seems like our mind melting caused us to forget that we had to update at the end of the loop... minor things like these, I had a feeling we were missing. Just want to add a test case with more than one sub-list to see if it works

--- src/eth/rlp.c
+++ src/eth/rlp.c
@@ -92,7 +92,7 @@ struct minth_rlp_t_parsing_data {
 void minth_rlp_t_parse_rlp_t(struct minth_rlp_t_parsing_data* pd) {
     if(*pd->b == '[') {
         if(pd->e - pd->b == 2) {
-            assert(*(pd->e) == ']');
+            assert(*(pd->e - 1) == ']');
             pd->token_type = MINTH_RLP_T_LIST_EMPTY_TOKEN;
         } else {
             pd->token_type = MINTH_RLP_T_LIST_FULL_TOKEN;
@@ -185,6 +185,9 @@ void minth_rlp_t_init_from_string_range(struct minth_rlp_t* t, const char* rlp_s
                 vector_rlp_t_push_back(&t->value.list, e);
                 if(ret == end) {
                     break;
+                } else {
+                    pd.b = ret;
+                    pd.e = end;
                 }
             }
             break;
@@ -193,66 +196,7 @@ void minth_rlp_t_init_from_string_range(struct minth_rlp_t* t, const char* rlp_s
             break;
     }
 }
-/*
-void minth_rlp_t_init_from_string_range(struct minth_rlp_t* t, const char* rlp_str_b, const char* rlp_str_e) {
-    //Check if we have a list!
-    if(*rlp_str_b == '[') {
-        //We have an empty list if it's just []
-        if(rlp_str_e - rlp_str_b == 2) {
-            //Assert the end, just for checking
-            assert(*(rlp_str_e - 1) == ']');
-            minth_rlp_t_init_list_empty(t);
-        } else {
-            //Work through each T in the list and recurse~ λ 
-            //NOTE: It might be neat to get the number of commas to know how much
-            //space to reserve! Just saying.
-            minth_rlp_t_init_list(t);
-            ++rlp_str_b;
-            const char* comm;
-            const char* space;
-            struct minth_rlp_t e;
-            for(;; rlp_str_b = comm + 1) {
-                algorithm_find(rlp_str_b, rlp_str_e, ',', comm);
-                if(comm == rlp_str_e) {
-                    //No more commas to zoom through! No more elements in the list, break!
-                    break;
-                }
-                //Advance to first non-space
-                for(; *rlp_str_b == ' '; ++rlp_str_b)
-                    ;
-                //Look for first non-space character to end, and parse!
-                algorithm_find(rlp_str_b, comm, ' ', space);
-                //We have a valid range [rlp_str_b, space), so:
-                minth_rlp_t_init_from_string_range(&e, rlp_str_b, space);
-                vector_rlp_t_push_back(&t->value.list, e);
-                
-            }
-            //No commas, but we still have one element regardless: 
-            //Advance to first non-space
-            for(; *rlp_str_b == ' '; ++rlp_str_b)
-                ;
-            //Look for first non-space character to end, and parse!
-            //ASSUMPTION: rlp_str_e - 1 is ']'
-            algorithm_find(rlp_str_b, rlp_str_e - 1, ' ', space);
-            //We have a valid range [rlp_str_b, space), so:
-            minth_rlp_t_init_from_string_range(&e, rlp_str_b, space);
-            vector_rlp_t_push_back(&t->value.list, e);
-        }
-    //Check if we have a byte array!
-    } else if(*rlp_str_b == '0') {
-        //Assert 0x prefix
-        assert(*(rlp_str_b + 1) == 'x');
-        if(rlp_str_e - rlp_str_b == 1) {
-            //An empty byte array, if just 0x!
-            minth_rlp_t_init_byte_array_empty(t);
-        } else {
-            //Note: This will break if last element is not part of the byte array! i.e. spaces,
-            //assume that each call is sanitized in terms of whitespace.
-            minth_rlp_t_init_byte_array_hexstring(t, rlp_str_b + 2, rlp_str_e);
-        }
-    }
-}
-*/
+
 /* TODO: Finish
 void minth_rlp_t_deinit(struct minth_rlp_t* t) {
 
